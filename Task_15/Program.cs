8cec6fd0a761a7b8cc22bfcb6a725295f3c6325b
unknown <nastena-alhovik@yandex.ru>
2022-09-04 16:40:26 +0300

    add Task 15 Homework 2

---
+++ Task_15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите цифру, обозначающую день недели (пн-1, вт-2, ср-3, чт-4, пт-5, сб-6, вс-7) ");
+int num_user = int.Parse(Console.ReadLine());
+
+if (num_user <6)
+{
+    Console.WriteLine("нет");
+}
+else
+{
+    if (num_user>7)
+    {
+    Console.WriteLine("введите цифру от 1 до 7");
+    }
+    else
+    {
+        Console.WriteLine("да"); 
+    }
+}
